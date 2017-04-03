a8d7628240eee20053bd948d9bbbc0618441e9b4
Andy <admin@ukiyoe.in>
2017-04-03 22:49:01 +1000

    Modify the Export Process

--- main.sh
+++ main.sh
@@ -1,22 +1,14 @@
 echo "Go to Neo4j folder ... "
-cd #neo4j folder path#
+cd ./neo4j-community-3.1.3/
 
-echo "Install neo4j-shell-tools"
+echo "Installation"
 curl "http://dist.neo4j.org/jexp/shell/neo4j-shell-tools_3.0.1.zip" -o neo4j-shell-tools.zip
 unzip neo4j-shell-tools.zip -d lib
 
 echo "Enable Neo4j shell-tool"
-chmod +x ./bin/neo4j-shell
-
-echo "Export GraphML file"
-./bin/neo4j start
-
-sleep 10s
-
+./bin/neo4j restart
 ./bin/neo4j-shell -c -file  ../export-sydney-gephi.cli
 
-sleep 10s
+sleep 5s
 
 ./bin/neo4j stop
-
-echo "End Exporting..."
